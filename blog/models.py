876a42640dbcba70c90c7ba1b48f54443df33e58
jzcat <1348842185@qq.com>
2024-01-25 18:04:48 +0800

    创建基本数据库表结构以及超级用户名和密码

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField("标题", max_length=100)
+    content = models.TextField("内容")
+    creatr_time = models.DateField("发布时间")
+    
